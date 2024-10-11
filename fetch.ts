049d915ce2a8717abf1ffa37c93d43ae3f2a2863
kixixixixi <kixixixixi@gmail.com>
2024-10-11 15:51:10 +0900

    Fix

--- fetch.ts
+++ fetch.ts
@@ -21,32 +21,44 @@ const fetchList = async ({
   const selectedPostionList = positionList.filter(
     ({ area_id }) => area_id == areaId
   )
-  const result = await Promise.all(
-    selectedPostionList.map(async (position) => {
-      const response = await fetch(
-        `https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat=${position.lat}&lon=${position.long}`
-      )
-      const result: {
-        results: {
-          muniCd: string
-          lv01Nm: string
-        }
-      } = await response.json()
-      return {
+
+  let results: { lat: number; long: number; address: string }[] = []
+  for (const position of selectedPostionList) {
+    const response = await fetch(
+      `https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat=${position.lat}&lon=${position.long}`
+    )
+    const result: {
+      results: {
+        muniCd: string
+        lv01Nm: string
+      }
+    } = await response.json()
+    results = [
+      ...results,
+      {
         lat: position.lat,
         long: position.long,
         address: result.results.lv01Nm,
-      }
-    })
-  )
+      },
+    ]
+  }
   await writeFile(
     `out/${name}.csv`,
-    result
-      .map(({ lat, long, address }) => [address, lat, long].join(","))
+    results
+      .map(({ lat, long, address }) =>
+        [
+          address,
+          lat,
+          long,
+          `"https://www.google.com/maps?q=${lat},${long}`,
+        ].join(",")
+      )
       .join("\n")
   )
-  await writeFile(`out/${name}.json`, JSON.stringify(result))
+  await writeFile(`out/${name}.json`, JSON.stringify(results))
 }
 
-fetchList({ areaId: 2, name: "sumida" })
-fetchList({ areaId: 5, name: "edoagwa" })
+;(async () => {
+  await fetchList({ areaId: 2, name: "sumida" })
+  await fetchList({ areaId: 5, name: "edoagwa" })
+})()
